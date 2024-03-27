bc64b96f7794e159cee055c21a616b3906b6344d
Nattyjr <88628695+Nattyjr@users.noreply.github.com>
2024-03-26 22:40:53 -0400

    genome.cpp

--- genome.cpp
+++ genome.cpp
@@ -3,15 +3,20 @@
 #include <cstdlib> // For rand() and srand()
 #include <ctime> // For time()
 
+
 using namespace std;
 
 // Constructor: Sets initial state with no chromosomes
+
+
 GeneticCode::GeneticCode() : chromosomes(nullptr), chromosomeCount(0) {}
 
 // Destructor: Ensures dynamic memory is freed
+
 GeneticCode::~GeneticCode() {
     clearChromosomes();
 }
+
 
 // Initialize chromosomes array with specified number of chromosomes
 void GeneticCode::initChromosomes(int numChromosomes) {
@@ -24,6 +29,7 @@ void GeneticCode::initChromosomes(int numChromosomes) {
     }
 }
 
+
 // Deallocate memory used for chromosomes
 void GeneticCode::clearChromosomes() {
     if (chromosomes != nullptr) {
@@ -32,6 +38,7 @@ void GeneticCode::clearChromosomes() {
         chromosomeCount = 0; // Reset count
     }
 }
+
 
 // Assign random RGB values to each chromosome
 void GeneticCode::randomizeAttributes() {
@@ -44,35 +51,42 @@ void GeneticCode::randomizeAttributes() {
     }
 }
 
+
 // Set the red intensity for a specific chromosome, ensuring index is within bounds
 void GeneticCode::setRedValue(int index, int value) {
     if (index >= 0 && index < chromosomeCount) chromosomes[index].red = value;
 }
+
 
 // Set the green intensity for a specific chromosome, ensuring index is within bounds
 void GeneticCode::setGreenValue(int index, int value) {
     if (index >= 0 && index < chromosomeCount) chromosomes[index].green = value;
 }
 
+
 // Set the blue intensity for a specific chromosome, ensuring index is within bounds
 void GeneticCode::setBlueValue(int index, int value) {
     if (index >= 0 && index < chromosomeCount) chromosomes[index].blue = value;
 }
+
 
 // Retrieve the red intensity from a specific chromosome, returns -1 if index is out of bounds
 int GeneticCode::getRedValue(int index) const {
     return (index >= 0 && index < chromosomeCount) ? chromosomes[index].red : -1;
 }
 
+
 // Retrieve the green intensity from a specific chromosome, returns -1 if index is out of bounds
 int GeneticCode::getGreenValue(int index) const {
     return (index >= 0 && index < chromosomeCount) ? chromosomes[index].green : -1;
 }
 
+
 // Retrieve the blue intensity from a specific chromosome, returns -1 if index is out of bounds
 int GeneticCode::getBlueValue(int index) const {
     return (index >= 0 && index < chromosomeCount) ? chromosomes[index].blue : -1;
 }
+
 
 
 // Display information about the genetic code including chromosome count and their RGB values
