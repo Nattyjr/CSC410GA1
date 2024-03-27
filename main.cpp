c4aae6b371569574fb6fe147ea42ea67ee4cd417
Nattyjr <88628695+Nattyjr@users.noreply.github.com>
2024-03-26 22:42:27 -0400

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 #include "genome.h"
 #include <iostream>
 
+
+
 int main() {
     GeneticCode myGeneticCode;
     
+  
     std::cout << "Initial genetic code state:" << std::endl;
     myGeneticCode.displayInfo();
 
+    
     myGeneticCode.initChromosomes(4);
     std::cout << "\nAfter initializing chromosomes:" << std::endl;
     myGeneticCode.displayInfo();
 
+    
     myGeneticCode.randomizeAttributes();
     std::cout << "\nAfter randomizing attributes:" << std::endl;
     myGeneticCode.displayInfo();
 
+    
     myGeneticCode.setBlueValue(0, 226);
     std::cout << "\nAfter setting the blue value of the first chromosome to 226:" << std::endl;
     myGeneticCode.displayInfo();
